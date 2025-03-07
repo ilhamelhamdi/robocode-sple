ad14e67d2dfe8c0c90f7a2f30c79d1c4182891f4
Ilham Abdillah Alhamdi <ilham.abdillah@ui.ac.id>
2025-03-08 00:53:05 +0700

    try use delta with Adudu character

---
+++ Adudu/src/elhamdi/Adudu.java
@@ -0,0 +1,25 @@
+package elhamdi;
+
+public class Adudu {
+	private String headShape;
+
+	public void setHeadShape() {
+		this.headShape = "Square";
+		System.out.println("Head shape set to: " + this.headShape);
+	}
+
+	public String getHeadShape() {
+		return this.headShape;
+	}
+
+	private String color;
+
+	public void setColor() {
+		this.color = "Green";
+		System.out.println("Color set to Green");
+	}
+
+	public String getColor() {
+		return this.color;
+	}
+}
